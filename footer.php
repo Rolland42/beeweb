7d41aecf3a4cd59b6387097faf316d15fa9df2d1
tiavinaralandison@gmail.com <tiavinaralandison@gmail.com>
2021-04-16 02:10:30 +0200

    feature/homepage branch updated

---
+++ footer.php
@@ -0,0 +1,26 @@
+    <footer class="container-fluid">
+        <div class="row">
+            <div class="col-md-4 col-sm-12 "></div>
+            <div class="col-md-4 col-sm-12 ">
+                <img src="images\webinar.jpg" alt=""> 
+            </div>
+            <div class="col-md-4 col-sm-12 "></div>
+        </div>
+
+        <div class="row">
+            <div class="col-md-4 col-sm-12 ">
+                <img src="images\icons-.jpg" alt=""> 
+            </div>
+            <div class="d-flex justify-content-center col-md-4 col-sm-12">
+                <img src="images\logo-bee-web-black.jpg" alt="">
+            </div>
+            <div id="rtl" class="col-md-4 col-sm-12">
+                <p>Contact</p>
+                <p>Licence</p>
+                <p>Mentions légales</p>
+            </div>
+        </div>
+    </footer>
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,76 @@
+<?php include "header.php"; ?>
+
+<!-- Start of HEADER -->
+<header class="container-fluid navbar navbar-expand-lg navbar-light">
+    <div class="container">
+        <!-- Start of LOGO -->
+        <div class="container">
+            <a class="navbar-brand" href="#">
+                <img src="images\logo-bee-web-black.jpg" alt="logo-du-blogue-bee-web">  
+            </a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+        </div>
+        <!-- End of LOGO -->
+
+        <!-- Start of NAVBAR -->
+        <nav class="collapse navbar-collapse" id="navbarText">
+            <span class="navbar-nav me-auto mb-2 mb-lg-0"></span>
+
+            <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="#">ACCUEIL</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">ARTICLES</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">CONTACT</a>
+                </li>
+            </ul>
+        </nav>
+        <!-- End of NAVBAR -->
+    </div>
+</header>
+<!-- End of HEADER -->
+
+<div id="main_content">
+    <div class="row">
+        <div class="col-md-4 col-sm-12"></div>
+        <div class="col-md-4 col-sm-12">
+            <h1>BEEWEB</h1>
+        </div>
+        <div class="col-md-4 col-sm-12"></div>
+    </div> 
+
+    <div class="row">
+        <div class="col-md-1 col-sm-12"></div>
+        <div class="col-md-4 col-sm-12">
+            <img id="main_image" src="images\video-conference.png" alt="" class="">
+        </div>
+        <div class="col-md-4 col-sm-12"></div>
+    </div>
+</div>
+
+<!-- MAIN Presentation -->
+<div class="container">
+    <div class="d-flex align-items-center">
+        <div class="row">
+            <div id="main_text">
+                <h2>Les métiers du web</h2>
+                <p>Le domaine du numérique se trouve au cœur de tous les secteurs d’activités , elle tient un grand rôle pour la visibilité d'une enseigne ou entreprise.</p>
+                <h2>Mode de travail</h2>
+                <h3>Les agences web , les agences de télécommunication ou autres :</h3>
+                <p>En générale , une agence web est composée d’une équipe de professionnels , experts dans leur domaine, qui accompagne son client pour son projet.<br>
+                L'équipe d'une agence web propose et assure le traitement d'un projet dans le délai défini afin de définir précisément le besoin et les objectifs qu'attend son client.<br>
+                À la suite de cette étape , l'équipe décidera et élaborera un devis sur-mesure, tout en respectant le budget du client.</p>
+                <h3>Freelancer :</h3>
+                <p>La révolution de l'auto-entreprenariat est l'origine de freelance dans le monde du travail . Notamment dans le numérique , le freelance est devenu un mode de travail qui influence beaucoup de jeune . D'une autre côté , </p>
+            </div>
+        </div>
+    </div>
+</div>
+
+<?php include "footer.php"; ?>

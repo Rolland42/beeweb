0a68ffc8220cba51ebe5f23ccbebc3280876ed45
sonia <s.rolland@codeur.online>
2021-05-04 15:51:04 +0200

    refonte page d'accueil

--- includes/header.php
+++ includes/header.php
@@ -28,7 +28,7 @@
           <span class="head-nav">
             <ul class="navbar-nav me-auto mb-2 mb-lg-0">
               <li class="nav-item">
-                  <a class="nav-link active" aria-current="page" href="#home">Home</a>
+                  <a class="nav-link active" aria-current="page" href="../pages/index.php">Home</a>
               </li>
               <li class="nav-item dropdown">
                 <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
---
+++ pages/draft.php
@@ -0,0 +1,134 @@
+<?php include "../includes/header.php";?>
+
+    <section id="home" class="container">
+        <div class="row g-0">
+            <div class="mt-5 padd-right col-sm-4 col-md-6">
+                <h1>LES MÉTIERS DU WEB</h1>
+                <div class="txt-justify mt-5">
+                    <p>Les métiers du web se trouvent  au cœur de tous les secteurs d’activités , elles tiennent  un grand rôle pour la visibilité d'une enseigne ou entreprise .</p>
+                    <p>On peut distingué le web en deux parties distinctes , les métiers du web liés à la production et à la gestion de contenu  et ceux qui sont autour des réseaux sociaux et au webmarketing.</p>
+                </div>
+            </div>
+            <div class="col-6 col-md-4">
+                <img src="../assets/images/presentation-beeweb.png" alt="">
+            </div>
+        </div>
+    </section>
+
+    <section id="articles" class="container">
+        <h2>Les métiers du web et ces différents types:</h2>
+    </section>
+    
+    <section class="container mt-5">
+        <div class="row row-cols-1 row-cols-md-3 g-5">
+
+            <div class="card m-3 p-3" style="max-width: 396px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                    <img src="../assets/images/webdesigner.png" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">WEB DESIGNER</h5>
+                        <p class="card-text">Un Web Designer tient un rôle important dans la production "interface utilisateur", car il est chargé de définir l’identité visuelle d’un site Internet et de ces services.</p>
+                    </div>
+                    </div>
+                    <div class="d-grid gap-2">
+                        <a href="web-designer.php"  class="btn btn-outline-secondary">Voir la fiche</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card m-3 p-3" style="max-width: 396px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                    <img src="../assets/images/uxdesigner.png" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">UX DESIGNER</h5>
+                        <p class="card-text">L'UX Designer travaille sur l'interactivité de la page ou site, ainsi que l'organisation et élément graphique d’une interface sur les navigateurs.</p>
+                    </div>
+                    </div>
+                    <div class="d-grid gap-2">
+                        <a href="ux-designer.php"  class="btn btn-outline-secondary">Voir la fiche</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card m-3 p-3" style="max-width: 396px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                    <img src="../assets/images/uidesigner.png" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">UI DESIGNER</h5>
+                        <p class="card-text">Un UI Designer est chargé plutôt sur l'organisation des éléments graphiques et textuels du contenu en répondant à des normes techniques ainsi qu’aux principes d’identité d’une marque..</p>
+                    </div>
+                    </div>
+                    <div class="d-grid gap-2">
+                        <a href="ui-designer.php"  class="btn btn-outline-secondary">Voir la fiche</a>
+                    </div>
+                </div>
+            </div>
+
+            <!-- row -->
+            
+            <div class="card m-3 p-3" style="max-width: 396px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                    <img src="../assets/images/respedit.png" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">RESPONSABLE ÉDITORIAL</h5>
+                        <p class="card-text">Le Responsable éditorial est en quelque sorte le superviseur du travail d’une ou de plusieurs équipes dans les domaines de la presse ou rédaction, de la communication de l’édition ou encore du web.</p>
+                    </div>
+                    </div>
+                    <div class="d-grid gap-2">
+                        <a href="resp-edi.php"  class="btn btn-outline-secondary">Voir la fiche</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card m-3 p-3" style="max-width: 396px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                    <img src="../assets/images/redac.png" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">RÉDACTEUR WEB</h5>
+                        <p class="card-text">Le Rédacteur Web produit de la rédaction pour le contenu web, en maîtrisant l’optimisation le référencement naturel "SEO" et peut travailler en totalement en autonomie sur un projet.</p>
+                    </div>
+                    </div>
+                    <div class="d-grid gap-2">
+                        <a href="redac-web.php"  class="btn btn-outline-secondary">Voir la fiche</a>
+                    </div>
+                </div>
+            </div>
+
+            <div class="card m-3 p-3" style="max-width: 396px;">
+                <div class="row g-0">
+                    <div class="col-md-4">
+                    <img src="../assets/images/uxwriter.png" alt="...">
+                    </div>
+                    <div class="col-md-8">
+                    <div class="card-body">
+                        <h5 class="card-title">UX WRITER</h5>
+                        <p class="card-text">L’UX Writer va donc travailler en étroite collaboration avec les UX et UI Designers du projet.</p>
+                    </div>
+                    </div>
+                    <div class="d-grid gap-2">
+                        <a href="ux-writer.php"  class="btn btn-outline-secondary">Voir la fiche</a>
+                    </div>
+                </div>
+            </div>
+
+
+
+        </div>
+    </section>
+
+    <?php include "../includes/footer.php" ?>

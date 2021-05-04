015c7ef3cdad525f86e9aa1cb7496a49b3ee64ea
sonia <s.rolland@codeur.online>
2021-05-04 18:31:26 +0200

    correction article

--- pages/com-on-line.php
+++ pages/com-on-line.php
@@ -23,7 +23,10 @@
                 </div>
                 <div class="col-sm-6 padding-top wow fadeInDown" data-wow-duration="1000ms" data-wow-delay="0ms">
                     <h2>Responsable communication on-line ou e-brand , de quoi s'agit-il?</h2>
-                    <p>Les responsables de la communication sont chargés de superviser toutes les communications internes et externes d'une entreprise, en veillant à ce que son message soit cohérent et attrayant.<br/>Également connu sous le nom de directeur de la communication, leurs principales tâches consistent à préparer des rapports détaillés sur les médias, des communiqués de presse et des documents de marketing.</p>
+                    <p>Les responsables de la communication sont chargés de superviser toutes les communications internes et externes d'une entreprise, en veillant à ce que son message soit cohérent et attrayant.
+                    <br/>Également connu sous le nom de directeur de la communication, leurs principales tâches consistent à préparer des rapports détaillés sur les médias, des communiqués de presse et des documents de marketing.
+                    </br>Il définit alors et décline la stratégie de communication sur les nouveaux médias en cohérence avec celle de l’entreprise. Il intervient aussi bien sur les outils de communication internes destinés aux employés que sur les outils de communication externes dédiés à la presse ou au grand public tels que les réseaux sociaux, 
+                         le site Internet de l'entreprise, ou encore la publicité on line, l’affiliation, les partenariats sur le web…</p>
                 </div>
             </div>
         </div>
